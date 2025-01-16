e963ce723099ccd22a5e1c081759e1efc4976f39
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2025-01-16 15:49:38 -0300

    calcBinarySize

---
+++ src/main/java/com/vinicin/API/Rest/controller/BinaryCalculatorController.java
@@ -0,0 +1,21 @@
+package com.vinicin.API.Rest.controller;
+
+import com.vinicin.API.Rest.service.BinaryCalculatorService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/binary")
+public class BinaryCalculatorController {
+    private final BinaryCalculatorService binaryCalculatorService;
+
+    public BinaryCalculatorController(BinaryCalculatorService binaryCalculatorService) {
+        this.binaryCalculatorService = binaryCalculatorService;
+    }
+
+    @GetMapping("/calc")
+    public int calc() {
+        return binaryCalculatorService.binaryCalculator(1010);
+    }
+}
--- src/main/java/com/vinicin/API/Rest/service/BinaryCalculatorService.java
+++ src/main/java/com/vinicin/API/Rest/service/BinaryCalculatorService.java
@@ -5,4 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class BinaryCalculatorService {
 
+    public int binaryCalculator(int binary){
+        String binaryString = String.valueOf(binary);
+        int binaryNumberSize = binaryString.length();
+        return binaryNumberSize;
+    }
 }
