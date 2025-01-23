eed3f802150d0908716c3c6fd09172b0b95b325e
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2025-01-23 16:02:30 -0300

    stylesChanged

--- front/interfaceGrafica/src/app/app.module.ts
+++ front/interfaceGrafica/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './_components/navbar/navbar.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { SidebarComponent } from './_components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
