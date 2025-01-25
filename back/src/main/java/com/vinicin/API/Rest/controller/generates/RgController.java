8e2d88b242c9c163a3deca20cdc9c8983765a60e
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2025-01-25 13:23:10 -0300

    generateRg

--- back/src/main/java/com/vinicin/API/Rest/controller/generates/RgController.java
+++ back/src/main/java/com/vinicin/API/Rest/controller/generates/RgController.java
@@ -1,4 +1,20 @@
 package com.vinicin.API.Rest.controller.generates;
 
+import com.vinicin.API.Rest.service.generates.RgGenerateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rg")
 public class RgController {
+    private final RgGenerateService rgGenerateService;
+    public RgController(RgGenerateService rgGenerateService) {
+        this.rgGenerateService = rgGenerateService;
+    }
+
+    @GetMapping("/generate")
+    public String generate() {
+        return rgGenerateService.gerarRg();
+    }
 }
--- back/src/main/java/com/vinicin/API/Rest/service/generates/RgGenerateService.java
+++ back/src/main/java/com/vinicin/API/Rest/service/generates/RgGenerateService.java
@@ -1,5 +1,63 @@
 package com.vinicin.API.Rest.service.generates;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
 public class RgGenerateService {
+
+    public String gerarRg() {
+        Random numeroAleatorio = new Random();
+
+        int[] numeros = new int[8];
+        for (int i = 0; i < 8; i++) {
+            numeros[i] = numeroAleatorio.nextInt(10);
+        }
+
+        int digitoVerificador = calcularDigitoVerificador(numeros);
+
+        StringBuilder rgGerado = new StringBuilder();
+        for (int numero : numeros) {
+            rgGerado.append(numero);
+        }
+        rgGerado.append(digitoVerificador);
+
+        return rgGerado.toString();
+    }
+
+    private int calcularDigitoVerificador(int[] numeros) {
+        int soma = 0;
+        int peso = 2;
+
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            soma += numeros[i] * peso;
+            peso++;
+        }
+        int resto = soma % 11;
+        int dv = 11 - resto;
+
+        if (dv == 10 || dv == 11) {
+            dv = 0;
+        }
+
+        return dv;
+    }
+
+    public boolean validarRg(String rg) {
+        if (rg == null || rg.length() != 9) {
+            return false;
+        }
+
+        int[] numeros = new int[8];
+        for (int i = 0; i < 8; i++) {
+            numeros[i] = Character.getNumericValue(rg.charAt(i));
+        }
+        int digitoVerificador = Character.getNumericValue(rg.charAt(8));
+
+        int dvCalculado = calcularDigitoVerificador(numeros);
+
+        return digitoVerificador == dvCalculado;
+    }
 
 }
