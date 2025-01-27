15e072b4c1dd045558ddbaf9f26b9791e54fdc23
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2025-01-27 14:37:47 -0300

    generatePassword

--- back/src/main/java/com/vinicin/API/Rest/controller/generates/PasswordController.java
+++ back/src/main/java/com/vinicin/API/Rest/controller/generates/PasswordController.java
@@ -1,9 +1,33 @@
 package com.vinicin.API.Rest.controller.generates;
 
+import com.vinicin.API.Rest.service.generates.GeneratePasswordService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/password")
 public class PasswordController {
+
+    private final GeneratePasswordService generatePasswordService;
+
+    public PasswordController(GeneratePasswordService generatePasswordService) {
+        this.generatePasswordService = generatePasswordService;
+    }
+
+    @GetMapping("/generate")
+    public String generatePassword(@RequestParam(defaultValue = "forte") String typeOfPassword,
+                                   @RequestParam(defaultValue = "8") int numberOfDigits) {
+        switch (typeOfPassword.toLowerCase()) {
+            case "fraca":
+                return generatePasswordService.generateWeakPassword(numberOfDigits);
+            case "media":
+                return generatePasswordService.generateAveragePassword(numberOfDigits);
+            case "forte":
+                return generatePasswordService.generateStrongPassword(numberOfDigits);
+            default:
+                throw new IllegalArgumentException("Invalid password type. Choose between 'fraca', 'media', or 'forte'.");
+        }
+    }
 }
--- back/src/main/java/com/vinicin/API/Rest/service/generates/GeneratePasswordService.java
+++ back/src/main/java/com/vinicin/API/Rest/service/generates/GeneratePasswordService.java
@@ -1,22 +1,42 @@
 package com.vinicin.API.Rest.service.generates;
 
 import org.springframework.stereotype.Service;
+import java.security.SecureRandom;
 
 @Service
 public class GeneratePasswordService {
-    private static String stringUpperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
-    private static String stringLowerCase = "qwertyuiopasdfghjklzxcvbnm";
-    private static String numbers = "0123456789";
-    private static String specialCharacters = "!@#$%¨&*()_-+=<>?/^";
-    
-    public String generateWeakPassword(String password) {
-        return "weakPassword";
-    }
-    public String generateAveragePassword(String password) {
-        return "averegePassword";
-    }
-    public String generateStrongPassword(String password) {
-        return "strongPassword";
+    private static final String stringUpperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    private static final String stringLowerCase = "qwertyuiopasdfghjklzxcvbnm";
+    private static final String numbers = "0123456789";
+    private static final String specialCharacters = "!@#$%¨&*()_-+=<>?/^";
+
+    private final SecureRandom random = new SecureRandom();
+
+    public String generateWeakPassword(int numberOfDigits) {
+        String characters = stringLowerCase + numbers;
+        return generatePassword(characters, numberOfDigits);
     }
 
+    public String generateAveragePassword(int numberOfDigits) {
+        String characters = stringLowerCase + stringUpperCase + numbers;
+        return generatePassword(characters, numberOfDigits);
+    }
+
+    public String generateStrongPassword(int numberOfDigits) {
+        String characters = stringLowerCase + stringUpperCase + numbers + specialCharacters;
+        return generatePassword(characters, numberOfDigits);
+    }
+
+    private String generatePassword(String characters, int length) {
+        if (length <= 0) {
+            throw new IllegalArgumentException("Number of digits must be greater than 0.");
+        }
+
+        StringBuilder password = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int index = random.nextInt(characters.length());
+            password.append(characters.charAt(index));
+        }
+        return password.toString();
+    }
 }
