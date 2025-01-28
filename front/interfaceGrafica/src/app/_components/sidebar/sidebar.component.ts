4325353914e9a5aa863f5b550dfdb4a981720bef
Vinicius Miguel <viniciusmiguelgit2@gmail.com>
2025-01-28 02:33:52 -0300

    cpfRouteAndAddTestImgs

--- front/interfaceGrafica/src/app/_components/sidebar/sidebar.component.ts
+++ front/interfaceGrafica/src/app/_components/sidebar/sidebar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: false,
-  
+
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
 
+  constructor(private router: Router) {}
+
+  navigateToCpfGenerate() {
+    this.router.navigate(['/cpf-generate']);
+  }
 }
--- front/interfaceGrafica/src/app/app-routing.module.ts
+++ front/interfaceGrafica/src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './_components/footer/footer.component';
+import { CpfGenerateComponent } from './cpf-generate/cpf-generate.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: '', component: HomeComponent},
+  {path: 'cpf-generate', component: CpfGenerateComponent}
 
 ];
 
